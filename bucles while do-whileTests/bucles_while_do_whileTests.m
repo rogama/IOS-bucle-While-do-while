d8c9b7454867bdb71c55dc7fcae36c81540f85c8
rober <gascon87@gmail.com>
2013-12-23 19:53:36 +0100

    Initial Commit

---
+++ bucles while do-whileTests/bucles_while_do_whileTests.m
@@ -0,0 +1,34 @@
+//
+//  bucles_while_do_whileTests.m
+//  bucles while do-whileTests
+//
+//  Created by rober on 23/12/13.
+//  Copyright (c) 2013 rogama. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bucles_while_do_whileTests : XCTestCase
+
+@end
+
+@implementation bucles_while_do_whileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
